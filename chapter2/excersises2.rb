aca528efb4352756cce280320de4564840e00dfe
Tyler Rollins <tylerrollins@outlook.com>
2013-07-19 16:18:02 -0700

    initial commit

---
+++ chapter2/excersises2.rb
@@ -0,0 +1,12 @@
+# hours in a year
+puts 365 * 24
+
+# minutes in a decade 
+puts 24*60*(365*10)
+
+# age in seconds (I am 24)
+puts 60*60*24*365*24
+
+# authors age
+puts 1160000000/(60*60*24*365)
+
