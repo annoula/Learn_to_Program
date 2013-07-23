7a930b996cc01561fa624996fd4bbcd1f6202966
Tyler Rollins <tylerrollins@outlook.com>
2013-07-22 19:47:08 -0700

    NA
    NA

---
+++ chapter8/sorting.rb
@@ -0,0 +1,13 @@
+# building and sorting an array
+
+puts 'Give me words! I will sort all that you enter.'
+words = []
+	while true
+		word = gets.chomp
+		if word == ''
+			break
+		end
+		words.push 
+	end
+puts 'Awesome! Here they are sorted!'
+puts words.sort
